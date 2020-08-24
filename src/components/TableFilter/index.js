0e856a92ab1c07adfc3c740d4012ac9c75c7bfda
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-24 13:02:07 -0500

    removed dynamic field selection. filters by dept

--- src/components/TableFilter/index.js
+++ src/components/TableFilter/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TableHeader from "../TableHeader";
 import Row from "../Row";
 import Col from "../Col";
-import {Button} from "react-bootstrap";
+import { Button} from "react-bootstrap";
 
 
 
@@ -10,51 +10,50 @@ const TableFilter = (props) => {
     const { employees } = props.state;
 
     const [filteredEmployees, setFilteredEmployees] = useState(employees);
-    const [searchInput, setSearchInput] = useState({key: 'Select field', search: ''});
+
+    const [searchInput, setSearchInput] = useState({ value: '', search: '' });
 
     const handleInputChange = event => {
-        setSearchInput({search: event.target.value});
+        setSearchInput({ search: event.target.value, value: 'department'});
         console.log(searchInput.search);
     };
+
+    // const handleFieldChange = event => {
+    //     setSearchInput({ value: event.target.value });
+    //     console.log(searchInput.value);
+    // }
 
     const handleFormFilter = event => {
         event.preventDefault();
 
-        setFilteredEmployees(employees.filter(employee => employee[searchInput.key] === searchInput.search));
+        setFilteredEmployees(employees.filter(employee => employee[searchInput.value] === searchInput.search));
         console.log(filteredEmployees);
-    }
-    const filterField = key => {
-        setSearchInput({key});
-    }
-    const handleFieldChange = event => {
-        setSearchInput({key: event.target.value });
-        console.log(searchInput.key);
     }
 
     return (
         <div>
-            {/* <form> */}
+            {/* <form onSubmit={handleFormFilter}> */}
             <Row>
-                <Col size="md-6">
-                <input
-                    value={searchInput.search}
-                    type="text"
-                    id="fieldInput"
-                    onChange={handleInputChange}
-                    
-                ></input>
+                <Col size="md-8">
+                    <input
+                        value={searchInput.search}
+                        type="text"
+                        id="fieldInput"
+                        onChange={handleInputChange}
+                        placeholder="filter by department"
+                    ></input>
                 </Col>
-                
-                <select value={searchInput.key} onChange={handleFieldChange}>
-                    <option value="Id" >Id</option>
-                    <option value="Name" >Name</option>
-                    <option value="Department" >Department</option>
-                    <option value="Email" >Email</option>
-                </select>
-              
+
+                {/* <DropdownButton id="dropdown-field" title="Select Field" onClick={handleFieldChange}>
+                    <Dropdown.Item value="id" >Id</Dropdown.Item>
+                    <Dropdown.Item value="name" >Name</Dropdown.Item>
+                    <Dropdown.Item value="department" >Department</Dropdown.Item>
+                    <Dropdown.Item value="email" >Email</Dropdown.Item>
+                </DropdownButton> */}
+
 
                 <Col size="md">
-                <Button variant="success" onClick={() => handleFormFilter()}>Filter</Button>
+                    <Button variant="success" onClick={handleFormFilter}>Filter</Button>
                 </Col>
             </Row>
             {/* </form> */}
