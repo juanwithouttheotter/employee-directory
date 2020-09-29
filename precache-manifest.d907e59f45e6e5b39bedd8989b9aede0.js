7be578457136cc5ec9db8858b9b5473d006e2935
Juan Osorio <juan.michael.osorio@gmail.com>
2020-09-28 21:22:57 -0500

    Updates

---
+++ precache-manifest.d907e59f45e6e5b39bedd8989b9aede0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96405ccaf0878efe441892e985dae6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e41f6dc41ab1e2775aa0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e380fb10a286a3d7efa6",
+    "url": "/static/css/main.039d12af.chunk.css"
+  },
+  {
+    "revision": "e41f6dc41ab1e2775aa0",
+    "url": "/static/js/2.49fb98a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.49fb98a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e380fb10a286a3d7efa6",
+    "url": "/static/js/main.0c527036.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
