5f85b1dd81b5639e7ae0c9ed6581a6e438c9e8e8
Juan Osorio <juan.michael.osorio@gmail.com>
2020-09-28 21:27:32 -0500

    Updates

---
+++ precache-manifest.3258bd30d3cd53be721d4dc7eb93fd3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da37a487c9cec0072e70b5dd13cb667b",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "e41f6dc41ab1e2775aa0",
+    "url": "/employee-directory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e380fb10a286a3d7efa6",
+    "url": "/employee-directory/static/css/main.039d12af.chunk.css"
+  },
+  {
+    "revision": "e41f6dc41ab1e2775aa0",
+    "url": "/employee-directory/static/js/2.49fb98a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/employee-directory/static/js/2.49fb98a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e380fb10a286a3d7efa6",
+    "url": "/employee-directory/static/js/main.0c527036.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d907e59f45e6e5b39bedd8989b9aede0.js"
+  "/employee-directory/precache-manifest.3258bd30d3cd53be721d4dc7eb93fd3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
